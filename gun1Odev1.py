9f7e66cdee9d29061d18969cbdcb5e24765a5abf
FurkanKtk <kutukfurkan@outlook.com>
2023-03-15 23:25:07 +0300

    Create gun1Odev1.py

---
+++ gun1Odev1.py
@@ -0,0 +1,66 @@
+"""
+**Python Veri Tipleri
+--String (Metin) Veri Tipleri
+Metinsel verileri saklamak istediğimiz zaman “String” veri tipine ihtiyaç duyarız.
+strOrnek = "metinsel ifade"
+
+--Numerik (Sayısal) Veri Tipleri
+Integer: Tam sayı değerleri barındırır.   
+Long:Int’in yetersiz olduğu yerlerde kullanılır
+Float: Ondalıklı sayı türünde değerler alır.
+
+--Sequence (Sıralama) Veri Tipleri
+List: -List veri tipi mutable yapıdadır yani araya farklı eleman ekleyebilir ve çıkarabiliriz
+      -liste içinde tüm elemanlar aynı veri tipinde olmak zorunda değildir. 
+      listOrnek = [1,1.5,aa] 
+
+Tuple:List yapısına benzer bir şekilde oluşan Tuple yapısı içerisinde yer alan verileri sadece okuyabiliriz.
+
+--Mapping (Haritalama) Veri Tipleri
+
+Dictionary olarak bilinen bu veri tipinde tanımlamalar küme parantez kullanılarak ve değer atamaları köşeli parantezler ile gerçekleştirilir.
+dictValue = {'ad' : 'Muhiddin', 'soyisim' : 'Dalveren', 'yas' : 36}
+
+--boolean veri tipi: 'true' ve 'false' değerlerini döndürür.
+
+--Set Veri Tipleri 
+Diğer veri tiplerini sırasız olarak bir arada tutmamızı sağlayan veri tipleri.
+kume = {4,5,"a","b"}
+
+
+Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+--kurs isimlerinin string 
+--eğitmen bölümleri string
+--yorum sayısı integer  şeklinde kullanılmış.
+
+Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+Profil kısmında bildirim iletilip iletmeyeceğini sorması.
+
+if bildirim_gonderilsin:
+    print("Mail iletildi)")
+
+else:
+    print("bildirim gönderme")
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
