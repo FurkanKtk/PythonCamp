50282a400c5da060c5bad8bfa21ae4272172a39d
FurkanKtk <kutukfurkan@outlook.com>
2023-03-31 20:28:58 +0300

    -

--- day5/test_day5.py
+++ day5/test_day5.py
@@ -2,27 +2,30 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec 
 
-class Test_Sauce:
+class Test_day5:
+    def _init_(self):
+        self.driver = driver=webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.maximize_window()
+        self.driver.get("https://www.saucedemo.com/")
+
+    def teardown_method(self):
+        self.driver.quit()
+
     def empty_username_password(self):
-        driver=webdriver.Chrome(ChromeDriverManager().install())
-        driver.maximize_window()
-        driver.get("https://www.saucedemo.com/")
-        sleep(5)
-        userNameInput=driver.find_element(By.ID,"user-name")
-        passwordInput=driver.find_element(By.ID,"password")
-        sleep(2)
+        WebDriverWait(self.driver,5).until()(ec.visibility_of_element_located((By.ID,"user-name")))
+        userNameInput=self.driver.find_element(By.ID,"user-name")
+        WebDriverWait(self.driver,5).until()(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput=self.driver.find_element(By.ID,"password")
         userNameInput.send_keys("")
         passwordInput.send_keys("")
-        sleep(2)
-        loginBtn=driver.find_element(By.ID,"login-button")
-        sleep(2)
+        loginBtn=self.driver.find_element(By.ID,"login-button")
         loginBtn.click()
         
-        errorMassage=driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]")
-        print(errorMassage)
-        testResult=errorMassage.text=="Username is required"
-        print(f"TEST SONUCU: {testResult}")
+        errorMassage=self.driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]")
+        assert errorMassage.text== "Username is required"
 
 
 
@@ -134,11 +137,11 @@ class Test_Sauce:
         closeBtn.click()
         sleep(30)
         
-testClass = Test_Sauce()
-testClass.empty_username_password()
-testClass.empty_password()
-testClass.locked_out()
-testClass.number_of_products()
-testClass.page()
+# testClass = Test_Sauce()
+# testClass.empty_username_password()
+# testClass.empty_password()
+# testClass.locked_out()
+# testClass.number_of_products()
+# testClass.page()
 
-testClass.error_with_X()
+# testClass.error_with_X()
